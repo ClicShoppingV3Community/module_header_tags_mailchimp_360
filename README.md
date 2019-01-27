64575f5e4e9edddc19d3ff25e370d7e6cb893504
ClicShopping - e-commerce Solution B2B B2C <39338579+ClicShopping@users.noreply.github.com>
2019-01-27 14:24:41 -0500

    new module

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # module_header_tags_mailchimp_360
+
 This module is designed to acquire the emails of customers, to send newsletters. but also to receive a set of statistics.
+
+Metatag provide information about a given Web page, most often to help search engines categorize them correctly. They are inserted into the HTML document, but more often are not directly visible to a user visiting the site.
+
+They have been a field of marketing research known as search engine optimization (Search Engine Optimization, SEO) name, in which we explore different methods to provide the website a user a better rank in search engines
+
+Via the installation system of ClicShopping administration  
+You must register on MailChimp
+
+Technical Prerequisites: None
+
+modules:
+- Compatibility: < version 3.0
+- Certified by e- Imaginis
+- Multi- languages
+
+Recommendation and documentation specific use :
+Il  est nécessaire de s'inscrire sur le site Mailchimp 360 et d'insérer le code de l'API dans ClicShopping
+
+Implementation:
+
+- Download the file that is in your control account / Information
+- In your Administration Tools / Installer module
+- In Step 2 : Choose fixed module
+- In step 3: choose your template or the module will be installed (if required )
+
+then
+
+Step 1: Configuration / Search Engine - networking  / meta data Activation
+Step 2: Click to see the modules
+Step 3 : Activate  Mailchimp 360 Module
+
+All informations about the ClicShopping
+
+ Download ClicShopping : https://github.com/ClicShopping/ClicShopping_V3/archive/master.zip
+
+ Community : https://www.clicshopping.org
+
+ Software : https://github.com/ClicShopping
+
+ Official add on : https://github.com/ClicShoppingOfficialModulesV3
+
+ Community add on : https://github.com/ClicShoppingV3Community
+
+ trademark License info : https://www.clicshopping.org/forum/trademark/ 
+
---
+++ includes/modules/header_tags/ht_mailchimp_360.php
@@ -0,0 +1,148 @@
+<?php
+/**
+ *
+ *  @copyright 2008 - https://www.clicshopping.org
+ *  @Brand : ClicShopping(Tm) at Inpi all right Reserved
+ *  @Licence GPL 2 & MIT
+ *  @licence MIT - Portion of osCommerce 2.4
+ *  @Info : https://www.clicshopping.org/forum/trademark/
+ *
+ */
+
+  use ClicShopping\OM\Registry;
+  use ClicShopping\OM\CLICSHOPPING;
+
+  class ht_mailchimp_360 {
+    public $code;
+    public $group;
+    public $title;
+    public $description;
+    public $sort_order;
+    public $enabled = false;
+
+    public function __construct() {
+      $this->code = get_class($this);
+      $this->group = basename(__DIR__);
+      $this->title = CLICSHOPPING::getDef('module_header_tags_mailchimp_360_title');
+      $this->description = CLICSHOPPING::getDef('module_header_tags_mailchimp_360_description');
+
+      if ( defined('MODULE_HEADER_TAGS_MAILCHIMP_360_STATUS') ) {
+        $this->sort_order = MODULE_HEADER_TAGS_MAILCHIMP_360_SORT_ORDER;
+        $this->enabled = (MODULE_HEADER_TAGS_MAILCHIMP_360_STATUS == 'True');
+      }
+    }
+
+    public function execute() {
+
+      $CLICSHOPPING_Template = Registry::get('Template');
+
+      include_once($CLICSHOPPING_Template->getModuleDirectory() . '/header_tags/ht_mailchimp_360/MCAPI.class.php');
+      include_once($CLICSHOPPING_Template->getModuleDirectory() . '/header_tags/ht_mailchimp_360/mc360.php');
+
+      $mc360 = new mc360();
+      $mc360->set_cookies();
+
+      if (isset($_GET['Checkout']) && isset($_GET['Success']))  {
+        $mc360->process();
+      }
+    }
+
+    public function isEnabled() {
+      return $this->enabled;
+    }
+
+    public function check() {
+      return defined('MODULE_HEADER_TAGS_MAILCHIMP_360_STATUS');
+    }
+
+    public function install() {
+      $CLICSHOPPING_Db = Registry::get('Db');
+
+      $CLICSHOPPING_Db->save('configuration', [
+          'configuration_title' => 'Enable MailChimp 360 Module',
+          'configuration_key' => 'MODULE_HEADER_TAGS_MAILCHIMP_360_STATUS',
+          'configuration_value' => 'True',
+          'configuration_description' => 'Do you want to activate this module in your shop?',
+          'configuration_group_id' => '6',
+          'sort_order' => '1',
+          'set_function' => 'clic_cfg_set_boolean_value(array(\'True\', \'False\'))',
+          'date_added' => 'now()'
+        ]
+      );
+
+      $CLICSHOPPING_Db->save('configuration', [
+          'configuration_title' => 'API Key',
+          'configuration_key' => 'MODULE_HEADER_TAGS_MAILCHIMP_360_API_KEY',
+          'configuration_value' => '',
+          'configuration_description' => 'An API Key assigned to your MailChimp account',
+          'configuration_group_id' => '6',
+          'sort_order' => '0',
+          'date_added' => 'now()'
+        ]
+      );
+
+      $CLICSHOPPING_Db->save('configuration', [
+          'configuration_title' => 'Debug E-Mail',
+          'configuration_key' => 'MODULE_HEADER_TAGS_MAILCHIMP_360_DEBUG_EMAIL',
+          'configuration_value' => '',
+          'configuration_description' => 'If an e-mail address is entered, debug data will be sent to it',
+          'configuration_group_id' => '6',
+          'sort_order' => '0',
+          'date_added' => 'now()'
+        ]
+      );
+
+      $CLICSHOPPING_Db->save('configuration', [
+          'configuration_title' => 'Sort Order',
+          'configuration_key' => 'MODULE_HEADER_TAGS_MAILCHIMP_360_SORT_ORDER',
+          'configuration_value' => '135',
+          'configuration_description' => 'Sort order of display. Lowest is displayed first.',
+          'configuration_group_id' => '6',
+          'sort_order' => '115',
+          'date_added' => 'now()'
+        ]
+      );
+
+// Internal parameters
+
+      $CLICSHOPPING_Db->save('configuration', [
+          'configuration_title' => 'MailChimp Store ID',
+          'configuration_key' => 'MODULE_HEADER_TAGS_MAILCHIMP_360_STORE_ID',
+          'configuration_value' => '',
+          'configuration_description' => 'Do not edit. Store ID value.',
+          'configuration_group_id' => '6',
+          'sort_order' => '0',
+          'date_added' => 'now()'
+        ]
+      );
+
+      $CLICSHOPPING_Db->save('configuration', [
+          'configuration_title' => 'MailChimp Key Valid',
+          'configuration_key' => 'MODULE_HEADER_TAGS_MAILCHIMP_360_KEY_VALID',
+          'configuration_value' => '',
+          'configuration_description' => 'Do not edit. Key Value value.',
+          'configuration_group_id' => '6',
+          'sort_order' => '0',
+          'date_added' => 'now()'
+        ]
+      );
+
+      return $CLICSHOPPING_Db->save('configuration', ['configuration_value' => '1'],
+                                              ['configuration_key' => 'WEBSITE_MODULE_INSTALLED']
+                            );
+    }
+
+    public function remove() {
+      Registry::get('Db')->query('delete from :table_configuration where configuration_key in ("' . implode('", "', $this->keys()) . '")');
+
+// Internal parameters
+      Registry::get('Db')->query('delete from :table_configuration where configuration_key in ("MODULE_HEADER_TAGS_MAILCHIMP_360_STORE_ID", "MODULE_HEADER_TAGS_MAILCHIMP_360_KEY_VALID")');
+    }
+
+    public function keys() {
+      return array('MODULE_HEADER_TAGS_MAILCHIMP_360_STATUS',
+                   'MODULE_HEADER_TAGS_MAILCHIMP_360_API_KEY',
+                   'MODULE_HEADER_TAGS_MAILCHIMP_360_DEBUG_EMAIL',
+                   'MODULE_HEADER_TAGS_MAILCHIMP_360_SORT_ORDER');
+    }
+  }
